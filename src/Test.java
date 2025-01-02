04414b2f150d70dcbb593e0a23d9a76584556f09
BT_2S14_19 <montasirn@nycstudents.net>
2025-01-02 10:16:36 -0500

    asdf

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,15 @@
 public class Test {
-
+private int num;
     public Test(){
+        num = 0;
+        System.out.println(num);
+    }
 
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
     }
 }
